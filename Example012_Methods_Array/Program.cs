b70dafd6b1ac73f884d8ce6e17a1d50001883534
Anastasiia <martishka85@list.ru>
2023-06-19 14:05:41 +0300

    add Example012_Methods сортировка массива

---
+++ Example012_Methods_Array/Program.cs
@@ -0,0 +1,37 @@
+﻿//Упорядочить массивы
+//Было:68321457
+//Результат:12345678
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array) //метод, который выводт массив
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort (int[] array) //метод, который упорядочивает массив
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) 
+            {
+                minPosition = j; //поиск минимального
+            }
+        }
+        int temporary = array[i]; //обменяли местами 2 переменные, через временную
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
