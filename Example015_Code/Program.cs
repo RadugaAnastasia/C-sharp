5f1df247b13796469496409d86f8f77fd97ad268
Anastasiia <martishka85@list.ru>
2023-07-05 16:39:17 +0300

    add Examples

---
+++ Example015_Code/Program.cs
@@ -0,0 +1,48 @@
+﻿//Посмотреть содержимое папки
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    } 
+}
+string path = "/Users/raduga/Downloads/02_LEARN/Geek Brains/C#/Examples/Example001_HelloConsole";
+DirectoryInfo di = new DirectoryInfo(path);
+CatalogInfo(path);
+
+// Игра в пирамидки
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+
+Towers();
+
+//Обход структур
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+//                 0    1    2    3     4    5    6    7    8    9    10   11
+void InOrderTraversa(int pos = 1)
+{
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNull0rEmpty(tree[left])) IsNull0rEmpty(left);
+        Console.WriteLine(tree[pos]);
+        if (right < tree.Length && !String.IsNull0rEmpty(tree[right])) IsNull0rEmpty(right);
+    }
+}
---
+++ Example016_PascalTriangle/Program.cs
@@ -0,0 +1,57 @@
+﻿int row = 5;
+int[,] triangle = new int[row, row];
+const int cellWidth = 3; //для вывода каждого элемента использовать 3 символа
+
+void FillTriangle()
+{
+    for (int i = 0; i < row; i++)
+    {
+        triangle[i, 0] = 1;
+        triangle[i, i] = 1;
+    }
+
+    for (int i = 2; i < row; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            triangle[i,j] = 
+                triangle[i-1, j-1] + triangle[i-1, j];
+        }
+    }
+}
+
+void  PrintTriangle()
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            if (triangle[i,j] != 0)
+            Console.Write($"{triangle[i,j], cellWidth}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Magic()
+{
+    int col = cellWidth * row;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            Console.SetCursorPosition(col, i + 1);
+            if (triangle[i,j] != 0) Console.Write($"{triangle[i,j], cellWidth}");
+            // if (triangle[i,j] != 0) Console.Write("*");
+            col += cellWidth * 2;
+        }
+        col = cellWidth * row - cellWidth * (i + 1);
+        Console.WriteLine();
+    }
+}
+
+Console.ReadLine();
+FillTriangle();
+PrintTriangle();
+Console.ReadLine();
+Magic();
